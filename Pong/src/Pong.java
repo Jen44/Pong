6c3b5902468ba831dcef936090b86b916a24f4d0
Jen44 <jenny.olb@hotmail.com>
2022-01-15 20:15:36 +1300

    Setting up main window that will display to play the game

--- Pong/src/Pong.java
+++ Pong/src/Pong.java
@@ -1,9 +1,21 @@
+import javax.swing.JFrame;
 
-public class Pong {
+public class Pong extends JFrame {
+
+	static final int SCREEN_WIDTH = 800;
+	static final int SCREEN_HEIGHT = 600;
+	static final String WINDOW_TITLE = "Pong";
+
+	public Pong() {
+		setTitle(WINDOW_TITLE);
+		setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
+		setResizable(false);
+		setVisible(true);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-//Test
+		new Pong();
 	}
 
 }
