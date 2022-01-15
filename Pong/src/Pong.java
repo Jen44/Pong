53fc0af766cbb0a422f01c1252581348f3b5e56f
Jen44 <jenny.olb@hotmail.com>
2022-01-15 23:01:37 +1300

    Created JPanel & future proofed game by removing 'magic numbers' and storing in static variables so can be updated later if needed

--- Pong/src/Pong.java
+++ Pong/src/Pong.java
@@ -1,21 +1,71 @@
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 public class Pong extends JFrame {
 
 	static final int SCREEN_WIDTH = 800;
 	static final int SCREEN_HEIGHT = 600;
 	static final String WINDOW_TITLE = "Pong";
+	static final Color BACKGROUND_COLOR = Color.BLACK;
 
 	public Pong() {
 		setTitle(WINDOW_TITLE);
 		setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
 		setResizable(false);
+		add(new PongPanel());
 		setVisible(true);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
 
+	public class PongPanel extends JPanel implements ActionListener, KeyListener {
+
+		public PongPanel() {
+
+			setBackground(BACKGROUND_COLOR);
+
+		}
+
+		@Override
+		public void keyTyped(KeyEvent event) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void keyPressed(KeyEvent event) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void keyReleased(KeyEvent event) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void actionPerformed(ActionEvent event) {
+			// TODO Auto-generated method stub
+
+		}
+
+	}
+
 	public static void main(String[] args) {
-		new Pong();
+		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+
+			public void run() {
+
+				new Pong();
+
+			}
+
+		});
 	}
 
 }
