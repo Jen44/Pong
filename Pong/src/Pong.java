cefc6442ee7571ab4a597e82f501573c5a8a9c5a
Jen44 <jenny.olb@hotmail.com>
2022-01-15 23:41:24 +1300

    Added timer and painting methods

--- Pong/src/Pong.java
+++ Pong/src/Pong.java
@@ -5,13 +5,15 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import javax.swing.Timer;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 
 public class Pong extends JFrame {
 
 	static final int SCREEN_WIDTH = 800;
 	static final int SCREEN_HEIGHT = 600;
 	static final String WINDOW_TITLE = "Pong";
-	static final Color BACKGROUND_COLOR = Color.BLACK;
 
 	public Pong() {
 		setTitle(WINDOW_TITLE);
@@ -24,9 +26,17 @@ public class Pong extends JFrame {
 
 	public class PongPanel extends JPanel implements ActionListener, KeyListener {
 
+		private final static Color BACKGROUND_COLOUR = Color.BLACK;
+
+		private final static int TIMER_DELAY = 5;
+
 		public PongPanel() {
 
-			setBackground(BACKGROUND_COLOR);
+			setBackground(BACKGROUND_COLOUR);
+
+			Timer timer = new Timer(TIMER_DELAY, this);
+
+			timer.start();
 
 		}
 
@@ -51,9 +61,24 @@ public class Pong extends JFrame {
 		@Override
 		public void actionPerformed(ActionEvent event) {
 			// TODO Auto-generated method stub
+			update();
+		}
+
+		private void update() {
 
 		}
 
+		@Override
+
+		public void paintComponent(Graphics g) {
+
+			super.paintComponent(g);
+
+			g.setColor(Color.WHITE);
+
+			g.fillRect(20, 20, 100, 100);
+
+		}
 	}
 
 	public static void main(String[] args) {
