60fdf60abe1a4025c9d6e99b1a16ba6a031b4c80
Jen44 <jenny.olb@hotmail.com>
2022-01-16 14:42:32 +1300

    Added sprite class

--- Pong/src/Pong.java
+++ Pong/src/Pong.java
@@ -1,13 +1,4 @@
 import javax.swing.JFrame;
-import javax.swing.JPanel;
-import java.awt.Color;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-import javax.swing.Timer;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
 
 public class Pong extends JFrame {
 
@@ -22,63 +13,6 @@ public class Pong extends JFrame {
 		add(new PongPanel());
 		setVisible(true);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
-	}
-
-	public class PongPanel extends JPanel implements ActionListener, KeyListener {
-
-		private final static Color BACKGROUND_COLOUR = Color.BLACK;
-
-		private final static int TIMER_DELAY = 5;
-
-		public PongPanel() {
-
-			setBackground(BACKGROUND_COLOUR);
-
-			Timer timer = new Timer(TIMER_DELAY, this);
-
-			timer.start();
-
-		}
-
-		@Override
-		public void keyTyped(KeyEvent event) {
-			// TODO Auto-generated method stub
-
-		}
-
-		@Override
-		public void keyPressed(KeyEvent event) {
-			// TODO Auto-generated method stub
-
-		}
-
-		@Override
-		public void keyReleased(KeyEvent event) {
-			// TODO Auto-generated method stub
-
-		}
-
-		@Override
-		public void actionPerformed(ActionEvent event) {
-			// TODO Auto-generated method stub
-			update();
-		}
-
-		private void update() {
-
-		}
-
-		@Override
-
-		public void paintComponent(Graphics g) {
-
-			super.paintComponent(g);
-
-			g.setColor(Color.WHITE);
-
-			g.fillRect(20, 20, 100, 100);
-
-		}
 	}
 
 	public static void main(String[] args) {
